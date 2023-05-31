b50c43f8e90aa5d188b8f7379c70dd005e49afe3
PJarczak <paweljarczak@int.pl>
2023-05-31 10:48:53 +0200

    .

--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
-require '../core/bootstrap.php';
+require_once '../vendor/autoload.php';
 
-$router = new Router;
+$router = new \Core\Router;
 
 require '../routes.php';
 
 $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
-require $router->direct($uri, $_SERVER['REQUEST_METHOD']);
+try {
+    $router->direct($uri, $_SERVER['REQUEST_METHOD']);
+} catch (Exception $e) {
+    // Obsługa błędów
+}
